7cd3316b07cb1c2f230ca7f3dc95797c7475a181
[EchoSwift-12] <[amtulrahmansadiya@gmail.com]>
2024-07-21 19:39:03 +0530

    commit-2

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import CreateCourse from "./components/createcourse";
 const  App = ()=>{
 
   return<>
-   <h1> App Component - Parent </h1>
-   <p>Below is the Create Course Component - Child </p>
    <CreateCourse/>
   </>
 }
--- src/components/createcourse.jsx
+++ src/components/createcourse.jsx
@@ -1,10 +1,24 @@
-const CreateCourse = () =>{
+import { useState } from "react"
 
+const CreateCourse = () => {
+    const [CourseName, setCourseName] = useState("")
+    const [CourseDescription, setCourseDescription] = useState("")
 
+    const changeCourseName = (e) => {
+        setCourseName(e.target.value)
+    }
+    const changeCourseDescription = (e) => {
+        setCourseDescription(e.target.value)
+    }
 
-    return<>
- <p> Create Course Component </p>
-</>
+    return <>
+        <input type="text" placeholder="Course Name"
+            onChange={changeCourseName} />
+        <p>{CourseName}</p>
 
+        <input type="text" placeholder="Course Description"
+            onChange={changeCourseDescription} />
+        <p>{CourseDescription}</p>
+    </>
 }
 export default CreateCourse
