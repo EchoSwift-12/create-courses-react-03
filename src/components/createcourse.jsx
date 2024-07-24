b9e00f8eca0e5cb8d8d21b2adcd4ff775c20cb19
[EchoSwift-12] <[amtulrahmansadiya@gmail.com]>
2024-07-24 12:39:40 +0530

    API FETCHING

--- src/components/createcourse.jsx
+++ src/components/createcourse.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { courseContext} from "../providers/contextproviders"
 
 const CreateCourse = () => {
     const [CourseName, setCourseName] = useState("")
     const [CourseDescription, setCourseDescription] = useState("")
+
+    const {CreateCourse} = useContext(courseContext) 
 
     const changeCourseName = (e) => {
         setCourseName(e.target.value)
@@ -10,13 +13,19 @@ const CreateCourse = () => {
     const changeCourseDescription = (e) => {
         setCourseDescription(e.target.value)
     }
+    const createCourseFn =()=>{
+        CreateCourse(CourseName, CourseDescription)
+        setCourseName("")
+        setCourseDescription("")
+    }
 
     return <>
-        <input type="text" placeholder="Course Name"
-            onChange={changeCourseName} />
+        <input type="text" placeholder="Enter Course Name"
+            onChange={changeCourseName}  value={CourseName}/>
 
         <input type="text" placeholder="Course Description"
-            onChange={changeCourseDescription} />
+            onChange={changeCourseDescription} value={CourseDescription}/>
+            <button onClick={createCourseFn}>Create Course</button>
     </>
 }
 export default CreateCourse
--- src/providers/contextproviders.jsx
+++ src/providers/contextproviders.jsx
@@ -5,8 +5,20 @@ const courseContext = createContext({
 })
 
 const CoursesContextProvider = ({ children }) => {
-    const createCourse = async () => {
 
+    const createCourse = async (CourseName, CourseDescription) => {
+        const response = await fetch('http://localhost:8000/courses', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                courseNameReact: CourseName,
+                CourseDescriptionReact: CourseDescription
+            })
+        })
+        const data = await response.json()
+        console.log(data.message);
     }
     return <>
         <courseContext.Provider value={{ createCourse }}>
